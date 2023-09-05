73bb46f4cbb38cdede9e631c71ccd3c9ff7407cd
Eason Shao <ShaoY@StPaulsSchool.org.uk>
2023-09-05 10:14:16 +0100

    update Task One.

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -5,6 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, L8!");
+
+        IntroTaskOne();
+
+        Console.ReadLine(); // Just to pause the program so that the terminal doesn't disappear suddenly
+    }
+
+    static void IntroTaskOne() // 05.09.2023 Task One
+    {
+        Console.WriteLine(
+            "*******\n" +
+            "*     *\n" +
+            "*  0  *\n" +
+            "* /|\\ *\n" +
+            "* / \\ *\n" +
+            "*******");
     }
 }
 
